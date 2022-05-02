8c6bd15a74dce64076a6303252d4fd301a30bda4
Francisco <panchovizcaino@gmail.com>
2022-05-02 13:42:24 -0500

    adding components back to src

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import { Nav } from '../components/nav'
-import { Hero } from '../components/hero'
-import { Faq } from '../components/info_section'
-import { Contact } from '../components/contact'
-import { Footer } from '../components/footer'
-import { Feature } from '../components/feature'
+import { Nav } from './src/components/nav'
+import { Hero } from './src/components/hero'
+import { Faq } from './src/components/info_section'
+import { Contact } from './src/components/contact'
+import { Footer } from './src/components/footer'
+import { Feature } from './src/components/feature'
 
 export default function Home() {
   return (
