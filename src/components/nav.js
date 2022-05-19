8e9c7135189608e3404b48aaf39786e9070947ad
panchovizcaino <panchovizcaino@gmail.com>
2022-05-19 17:06:59 -0500

    navbar changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,7 +9,7 @@ export const Nav = () => {
     return (
         <div className="bg-light-green-900 shadow-lg">
             <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div className="relative flex items-center justify-between">                   
+                <div className="relative flex items-end justify-between">                   
                             <Image
                                 src={logo}
                                 alt="Company Logo"
